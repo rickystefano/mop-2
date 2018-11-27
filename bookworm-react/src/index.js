8c744a497370307ece00559e4275821fb96f49d4
Ricky Simadinata <528101@student.glu.nl>
2018-11-27 09:36:34 +0100

    dependecies from redux installed

--- bookworm-react/src/index.js
+++ bookworm-react/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+const createStore = store => {};
 
 ReactDOM.render(
   <BrowserRouter>
